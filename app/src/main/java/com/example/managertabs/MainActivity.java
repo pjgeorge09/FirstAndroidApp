ba4d10f31528450312b885149405dbf3a84c5453
Bryan Hilldrup <hilldrupbf@vcu.edu>
2019-03-14 18:33:44 -0400

    Four main icons applied to navigation bar

--- app/src/main/java/com/example/managertabs/MainActivity.java
+++ app/src/main/java/com/example/managertabs/MainActivity.java
@@ -80,15 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) { // TODO Rename camera, gallery, etc
+        if (id == R.id.nav_inventory) { // TODO Rename camera, gallery, etc
             // Handle the camera action
             // Transition to new activity
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_staff) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_donors) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_donations) {
 
         } else if (id == R.id.nav_share) {
 
