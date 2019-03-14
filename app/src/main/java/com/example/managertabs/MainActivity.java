9acae2ce10fa1a62f63ef4c6039b79682c9b4296
Bryan Hilldrup <hilldrupbf@vcu.edu>
2019-03-14 18:39:08 -0400

    Added email link/icon

--- app/src/main/java/com/example/managertabs/MainActivity.java
+++ app/src/main/java/com/example/managertabs/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_email) {
 
         }
 
