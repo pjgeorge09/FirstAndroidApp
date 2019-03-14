783a907c0edbb80200ad90166856613680fb310f
Bryan Hilldrup <hilldrupbf@vcu.edu>
2019-03-14 16:11:38 -0400

    Added storage icon png to drawable

--- app/src/main/java/com/example/managertabs/WorkerHomeScreen.java
+++ app/src/main/java/com/example/managertabs/WorkerHomeScreen.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// Workers Home Screen Class
 public class WorkerHomeScreen extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
