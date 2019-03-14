3725a41134ae34932011a1b2e4761326ef8f786d
AveryRector <rectoravery@gmail.com>
2019-03-14 17:33:02 -0400

    Login screen basic

--- app/src/main/java/com/example/managertabs/LoginTemp.java
+++ app/src/main/java/com/example/managertabs/LoginTemp.java
@@ -7,41 +7,64 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginTemp extends AppCompatActivity {
 
-    public static final String FOO = "FOO";
-    public static final String BAR = "BAR";
+    private EditText Email;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_temp);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Email = (EditText)findViewById(R.id.etEmail1);
+        Password = (EditText)findViewById(R.id.etPassword1);
+        Info = (TextView)findViewById(R.id.textView2);
+        Login = (Button)findViewById(R.id.btnLogin1);
+
+        Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                validate(Email.getText().toString(), Password.getText().toString());
             }
         });
+
+
     }
 
-    public void onClickManager(View view){
+    /*
+        Needs to pull valid user names and passwords from data base
+        Needs to direct to correct page (manager or users) if valid login
+     */
+    private void validate(String userEmail, String userPassword) {
+        if(userEmail.equals("manager") && userPassword.equals("manager")) {
+            Intent intent = new Intent(LoginTemp.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else if(userEmail.equals("worker") && userPassword.equals("worker")){
+            Intent intent1 = new Intent(LoginTemp.this, WorkerHomeScreen.class);
+            startActivity(intent1);
+        }
+        else {
+            counter--;
 
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            Info.setText("Number of attempts reamining: " + String.valueOf(counter));
 
-        startActivity(intent);
-    }
+            if(counter == 0) {
+                Login.setEnabled(false);
+            }
+        }
 
-    public void onClickWorker(View view){
-
-        Intent intent = new Intent(getApplicationContext(), WorkerHomeScreen.class);
-
-        startActivity(intent);
     }
 }
