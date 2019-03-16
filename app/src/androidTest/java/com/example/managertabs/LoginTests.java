9666443827441151af25c5ee4c61828d9aee299a
Bryan Hilldrup <hilldrupbf@vcu.edu>
2019-03-16 15:27:43 -0400

    Two tests, logs into app as manager and worker

--- app/src/androidTest/java/com/example/managertabs/LoginTests.java
+++ app/src/androidTest/java/com/example/managertabs/LoginTests.java
@@ -1,11 +1,15 @@
 package com.example.managertabs;
 
+import static android.support.test.espresso.intent.Intents.*;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.*;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.Test;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -16,17 +20,33 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 
+
 @RunWith(AndroidJUnit4.class)
 public class LoginTests {
-    @Rule
+  /*  @Rule
     public ActivityTestRule<Login> loginActivityTestRule
             = new ActivityTestRule<>(Login.class);
+            */
+
+    @Rule
+    public IntentsTestRule<Login> loginActivityTestRule
+            = new IntentsTestRule<>(Login.class);
 
     @Test
-    public void testLogin(){
+    public void testLoginManager(){
         onView(withId(R.id.etEmail1)).perform(typeText("manager"));
         onView(withId(R.id.etPassword1)).perform(typeText("manager"),
                 closeSoftKeyboard());
         onView(withText("LOGIN")).perform(click());
+        intended(hasComponent(ManagerHomeScreen.class.getName()));
+    }
+
+    @Test
+    public void testLoginWorker(){
+        onView(withId(R.id.etEmail1)).perform(typeText("worker"));
+        onView(withId(R.id.etPassword1)).perform(typeText("worker"),
+                closeSoftKeyboard());
+        onView(withText("LOGIN")).perform(click());
+        intended(hasComponent(WorkerHomeScreen.class.getName()));
     }
 }
