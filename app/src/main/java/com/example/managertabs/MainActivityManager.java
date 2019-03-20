c96731f29e3fd2ae209cc98ad0cae89d834af369
Pete George <georgepj@vcu.edu>
2019-03-20 15:01:52 -0400

    working on creating mains

---
+++ app/src/main/java/com/example/managertabs/MainActivityManager.java
@@ -0,0 +1,102 @@
+package com.example.managertabs;
+
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivityManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /* Sets the back button to revert to the last screen. In the case that the drawer is open, it simply closes the drawer instead. */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // If drawer (tabs) are open, close them
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        // If drawer (tabs) are closed, revert to last activity via super.onBackPressed method
+        else {
+            Intent intent = new Intent(getApplicationContext(), ManagerHomeScreen.class);
+            startActivity(intent);
+            //super.onBackPressed();
+        }
+    }
+
+    /* Creates the inflater for the Setting menu button. */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    /* Method to be instantiated later, will listen for buttons in settings that are not yet designated */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.logout) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            // The below code clears the stack so the activity cannot be reached. (Security bug cleared) (I.E. ERASE STACK MEMORY)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* Method used when drawer (tabs) layout is open, listens for button clicks (tab selected) and
+    does a screen transition based on received */
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        // If INVENTORY is selected by manager, go to Inventory.class
+        if (id == R.id.nav_inventory) {
+            Intent intent = new Intent(getApplicationContext(), Inventory.class);
+            startActivity(intent);
+        }
+        // If DONATIONS is selected by manager, go to Donations.class
+        else if (id == R.id.nav_donations) {
+            Intent intent = new Intent(getApplicationContext(), Donations.class);
+            startActivity(intent);
+        }
+        // If DONORS is selected by manager, Donors.class
+        else if (id == R.id.nav_donors) {
+            Intent intent = new Intent(getApplicationContext(), Donors.class);
+            startActivity(intent);
+        }
+        // If STAFF is selected by manager, go to Staff.class
+        else if (id == R.id.nav_staff) {
+            Intent intent = new Intent(getApplicationContext(), Staff.class);
+            startActivity(intent);
+        }
+        // If ______ is selected by manager, go to ______
+        else if (id == R.id.nav_email) {
+        }
+        // If ______ is selected by manager, go to ______
+        else if (id == R.id.nav_send) {
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
