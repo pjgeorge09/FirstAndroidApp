5e518d8f31797c01ee7bfb6d878f4c02eae8418c
poi94 <gullerpj@mymail.vcu.edu>
2019-04-10 19:33:28 -0400

    added drop down arrow non functional

--- app/src/androidTest/java/com/example/managertabs/Donation/DonationTest.java
+++ app/src/androidTest/java/com/example/managertabs/Donation/DonationTest.java
@@ -2,6 +2,7 @@ package com.example.managertabs.Donation;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 
@@ -9,66 +10,69 @@ import static org.junit.Assert.*;
 
 public class DonationTest {
     //makes a donation object to test getters and setter
-    Donation test =new Donation("03/22/1901", "Beans", "500", "Small");
+    Donation test = new Donation("03/22/1901", "Beans", "500", "Small");
 
     @Before
     //makes a donation object to test getters and setter
-    public void setUp()  {
-      Donation test =new Donation("03/22/1901", "Beans", "500", "Small");
+    public void setUp() {
+        Donation test = new Donation("03/22/1901", "Beans", "500", "Small");
 
     }
 
     @Test
     public void getDate() {
-        String getDate=test.getDate();
-        assertEquals(getDate,"03/22/1901");
+        String getDate = test.getDate();
+        assertEquals(getDate, "03/22/1901");
     }
 
     @Test
     public void setDate() {
         test.setDate("01/01/1920");
-        String getDate=test.getDate();
-        assertEquals(getDate,"01/01/1920");
+        String getDate = test.getDate();
+        assertEquals(getDate, "01/01/1920");
     }
 
     @Test
     public void getItem() {
-        String getItem=test.getItem();
-        assertEquals(getItem,"Beans");
+        String getItem = test.getItem();
+        assertEquals(getItem, "Beans");
 
     }
 
     @Test
     public void setItem() {
         test.setItem("Pizza");
-        String getItem=test.getItem();
-        assertEquals(getItem,"Pizza");
+        String getItem = test.getItem();
+        assertEquals(getItem, "Pizza");
     }
 
     @Test
     public void getQuantity() {
-        String getItem=test.getQuantity();
-        assertEquals(getItem,"500");
+        String getItem = test.getQuantity();
+        assertEquals(getItem, "500");
     }
 
     @Test
     public void setQuantity() {
         test.setQuantity("50");
-        String getItem=test.getQuantity();
-        assertEquals(getItem,"50");
+        String getItem = test.getQuantity();
+        assertEquals(getItem, "50");
     }
 
     @Test
     public void getSize() {
-        String getItem=test.getSize();
-        assertEquals(getItem,"Small");
+        String getItem = test.getSize();
+        assertEquals(getItem, "Small");
     }
 
     @Test
     public void setSize() {
         test.setSize("Large");
-        String getItem=test.getSize();
-        assertEquals(getItem,"Large");
+        String getItem = test.getSize();
+        assertEquals(getItem, "Large");
 
     }
+
+    //Second Batch of tests!!!!!!!!!!!!!
+    //?!!!!!!!!!!!!!!!
 }
--- app/src/main/java/com/example/managertabs/Donation/Donation.java
+++ app/src/main/java/com/example/managertabs/Donation/Donation.java
@@ -20,34 +20,59 @@ public  Donation()
     }
 //getters and setter for each field above
     public String getDate() {
-        return Date;
+    if(Date==null){
+        throw new IllegalArgumentException();
+    }
+    return Date;
     }
 
     public void setDate(String date) {
+        if(date==null){
+            throw new IllegalArgumentException();
+        }
         Date = date;
     }
 
     public String getItem() {
+        if(Item==null){
+            throw new IllegalArgumentException();
+        }
         return Item;
     }
 
     public void setItem(String item) {
+        if(item==null){
+            throw new IllegalArgumentException();
+        }
         Item = item;
     }
 
     public String getQuantity() {
+        if(Quantity==null){
+            throw new IllegalArgumentException();
+        }
+
         return Quantity;
     }
 
     public void setQuantity(String quantity) {
+        if(quantity==null){
+            throw new IllegalArgumentException();
+        }
         Quantity = quantity;
     }
 
     public String getSize() {
+        if(Size==null){
+            throw new IllegalArgumentException();
+        }
         return Size;
     }
 
     public void setSize(String size) {
+        if(size==null){
+            throw new IllegalArgumentException();
+        }
         Size = size;
     }
 }
