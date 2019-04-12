96869005355f2ea7391f04b77fb9fabe6f7a4f85
poi94 <gullerpj@mymail.vcu.edu>
2019-04-12 00:15:49 -0400

    Donation adapter modified for drop downs

--- app/src/main/java/com/example/managertabs/Donation/DonationsAdapter.java
+++ app/src/main/java/com/example/managertabs/Donation/DonationsAdapter.java
@@ -1,15 +1,19 @@
 package com.example.managertabs.Donation;
 
+import android.animation.ObjectAnimator;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.managertabs.R;
+import com.example.managertabs.inventoryData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +22,19 @@ import java.util.Locale;
 public class DonationsAdapter extends RecyclerView.Adapter<DonationsAdapter.DonationViewHolder>{
 
     List<Donation> ProperData;
-//allow the array lis to pass to the rest of the adapter
+//allow the array list to pass to the rest of the adapter
+
+    SparseBooleanArray DonationExpandState = new SparseBooleanArray();
+    // SparseBooleanArray maps a list of integers to a corresponding boolean
+
+
+    //allow the array list to pass to the rest of the adapter
     public DonationsAdapter(List<Donation> ProperData) {
         this.ProperData = ProperData;
+        for (int i = 0; i < ProperData.size(); i++) {
+            DonationExpandState.append(i, false);
+        }
+
     }
 
     //Creates Custom ViewHolder
@@ -31,11 +45,33 @@ public class DonationsAdapter extends RecyclerView.Adapter<DonationsAdapter.Dona
         DonationViewHolder pvh = new DonationViewHolder(v);
         return pvh;
     }
+    //Code to rotate button
+    private ObjectAnimator createRotateAnimator(final View target, final float from, final float to) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(target, "rotation", from, to);
+        animator.setDuration(300);
+        animator.setInterpolator(new LinearInterpolator());
+        return animator;
+    }
+    // Method to handle changing layouts based on clicking the expandable arrow button
+    private void onClickButton(final LinearLayout expandableLayout, final RelativeLayout buttonLayout, final  int i) {
+
+        //Simply set View to Gone if not expanded
+        //Not necessary but I put simple rotation on button layout
+        if (expandableLayout.getVisibility() == View.VISIBLE){
+            createRotateAnimator(buttonLayout, 90f, 0f).start();
+            expandableLayout.setVisibility(View.GONE);
+            DonationExpandState.put(i, false);
+        }else{
+            createRotateAnimator(buttonLayout, 0f, 90f).start();
+            expandableLayout.setVisibility(View.VISIBLE);
+            DonationExpandState.put(i, true);
+        }
+    }
 
 
     //specify contents of each item
     @Override
-    public void onBindViewHolder(DonationViewHolder holder, int position){
+    public void onBindViewHolder(final DonationViewHolder holder, final int position){
    //passes the data to the text views
         //inventory
         holder.Category.setText("Category: "+ProperData.get(position).inventoryCategory);
@@ -46,7 +82,36 @@ public class DonationsAdapter extends RecyclerView.Adapter<DonationsAdapter.Dona
         holder.DateR.setText("Date: "+ ProperData.get(position).inventoryDateRecived);
         holder.Min_Threshold.setText("Minimum Threshold: "+ProperData.get(position).inventoryMin_Threshold);
         //Donor
-        holder
+        holder.DonorName.setText("Name: "+ProperData.get(position).DonorName);
+        holder.DonorPhone.setText("Phone:"+ProperData.get(position).DonorPhone);
+        holder.DonorEmail.setText("Email:"+ProperData.get(position).DonorEmail);
+
+
+        // Check if view is expanded and uses a ternary operation to set the view accordingly
+
+        final boolean isExpanded = DonationExpandState.get(position);
+        holder.inventoryExpandableLayout.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+
+        final boolean isExpanded2 = DonationExpandState.get(position);
+        holder.DonorExpandavleLayour.setVisibility(isExpanded2?View.VISIBLE:View.GONE);
+//Inventory-----------------------------------------------------------------------------------------
+        // Uses a ternary operator to set the buttons rotation based on the view state
+        holder.inventoryButtonLayout.setRotation(DonationExpandState.get(position) ? 180f : 0f);
+        holder.inventoryButtonLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                onClickButton(holder.inventoryExpandableLayout, holder.inventoryButtonLayout, position);
+            }
+        });
+        //Donor---------------------------------------------------------------------------------------
+        // Uses a ternary operator to set the buttons rotation based on the view state
+        holder.DonorButtonLayout.setRotation(DonationExpandState.get(position) ? 180f : 0f);
+        holder.DonorButtonLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                onClickButton(holder.DonorExpandavleLayour, holder.DonorButtonLayout, position);
+            }
+        });
     }
 
     @Override
@@ -77,19 +142,36 @@ public class DonationsAdapter extends RecyclerView.Adapter<DonationsAdapter.Dona
     TextView DonorName;
     TextView DonorPhone;
     TextView DonorEmail;
+
     RelativeLayout inventoryButtonLayout;
+    RelativeLayout DonorButtonLayout;
+
     LinearLayout inventoryExpandableLayout;
+    LinearLayout DonorExpandavleLayour;
         //links the card view and textboxes to the view holder
         public DonationViewHolder(View itemView) {
             super(itemView);
 
             cardView = (CardView)itemView.findViewById(R.id.CV_donation);
             //text box in row
-            Date= (TextView)itemView.findViewById(R.id.Date);
-            Item= (TextView)itemView.findViewById(R.id.Item);
-            Quantity= (TextView)itemView.findViewById(R.id.Quantity);
-            Size= (TextView)itemView.findViewById(R.id.Size);
-
+            //Inventory
+            Category=(TextView)itemView.findViewById(R.id.Donation_Category);
+            Expire=(TextView)itemView.findViewById(R.id.Donation_Expire);
+            Item= (TextView)itemView.findViewById(R.id.Donation_Item);
+            Location=(TextView)itemView.findViewById(R.id.Donation_Location);
+            Quantity= (TextView)itemView.findViewById(R.id.Donation_Quantity);
+            DateR= (TextView)itemView.findViewById(R.id.Donation_Date_Received);
+            Min_Threshold= (TextView)itemView.findViewById(R.id.Donation_Min);
+            //button stuff
+            inventoryButtonLayout = (RelativeLayout) itemView.findViewById(R.id.Donation_buttonDropDown_Inventory);
+            inventoryExpandableLayout = (LinearLayout) itemView.findViewById(R.id.Donation_expand);
+            //Donor
+            DonorName=(TextView)itemView.findViewById(R.id.Donation_Donor_Name);
+            DonorPhone=(TextView)itemView.findViewById(R.id.Donation_Donor_Phone);
+            DonorEmail=(TextView)itemView.findViewById(R.id.Donation_Donor_Email);
+            //ButtonStuff
+            DonorButtonLayout=(RelativeLayout) itemView.findViewById(R.id.Donation_buttonDropDown_Donors);
+            DonorExpandavleLayour= (LinearLayout) itemView.findViewById(R.id.Donation_Donor_Expand);
 
         }
     }
